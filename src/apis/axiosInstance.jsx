19538b571d6b17b0b4b3076d29ef14b67282b468
AnVT <58844911+VTAIT@users.noreply.github.com>
2023-11-23 14:05:24 +0700

    Update axiosInstance.jsx

--- src/apis/axiosInstance.jsx
+++ src/apis/axiosInstance.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001/api/v1";
+const BASE_URL = "https://restaurant-app-0xyk.onrender.com/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
